df5c9082182bc236000bfecefa4e767c8a821d12
BorisPanich <panich2303@gmail.com>
2021-09-06 11:10:52 +0300

    refactored WorkingConditions

--- src/myWorks/MyWorks.js
+++ src/myWorks/MyWorks.js
@@ -3,6 +3,7 @@ import s from './MyWorks.module.scss';
 import one from '../assets/images/projects/giris.jpeg';
 import {Title} from "../Common/components/title/Title";
 import MyWork from "./myWork/MyWork";
+import Fade from 'react-reveal/Fade';
 
 export const MyWorks = React.memo(() => {
     const oneWork = {
@@ -12,36 +13,38 @@ export const MyWorks = React.memo(() => {
     return (
         <div className={s.worksBlock}>
             <div className={s.container} id='works'>
-                <Title title={'My works'}
-                       titleDescription={'Check out my latest web software development portfolio projects.'}
-                       id='works'
-                />
-                <div className={s.works}>
-                    <MyWork style={oneWork}
-                            link={'https://BorisPanich.github.io/React-project'}
-                            repLink={'https://github.com/BorisPanich/React-project'}
-                            projectName={'Social Network'}
-                            description={
-                                'It is the most biggest project which I refresh every month and learning new technologies. It helps me to be in trends.'
-                            }
+                <Fade right>
+                    <Title title={'My works'}
+                           titleDescription={'Check out my latest web software development portfolio projects.'}
+                           id='works'
                     />
-                    <MyWork style={oneWork}
-                            link={'https://BorisPanich.github.io/TodoList'}
-                            repLink={'https://github.com/BorisPanich/TodoList'}
-                            projectName={'Todo List'}
-                            description={
-                                'Redux-toolkit contains the best practice from all React developers. This project shows redux-toolkit and his strict limits.'
-                            }
-                    />
-                    <MyWork style={oneWork}
-                            link={''}
-                            repLink={'https://github.com/BorisPanich/react_KKP'}
-                            projectName={'Storybook miniProject'}
-                            description={
-                                'Project using storybook'
-                            }
-                    />
-                </div>
+                    <div className={s.works}>
+                        <MyWork style={oneWork}
+                                link={'https://BorisPanich.github.io/React-project'}
+                                repLink={'https://github.com/BorisPanich/React-project'}
+                                projectName={'Social Network'}
+                                description={
+                                    'It is the most biggest project which I refresh every month and learning new technologies. It helps me to be in trends.'
+                                }
+                        />
+                        <MyWork style={oneWork}
+                                link={'https://BorisPanich.github.io/TodoList'}
+                                repLink={'https://github.com/BorisPanich/TodoList'}
+                                projectName={'Todo List'}
+                                description={
+                                    'Redux-toolkit contains the best practice from all React developers. This project shows redux-toolkit and his strict limits.'
+                                }
+                        />
+                        <MyWork style={oneWork}
+                                link={''}
+                                repLink={'https://github.com/BorisPanich/react_KKP'}
+                                projectName={'Storybook miniProject'}
+                                description={
+                                    'Project using storybook'
+                                }
+                        />
+                    </div>
+                </Fade>
             </div>
         </div>
     )
--- src/workingConditions/WorkingConditions.js
+++ src/workingConditions/WorkingConditions.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import s from './WorkingConditions.module.css';
-import sContainer from '../Common/styles/Container.module.scss'
+import s from './WorkingConditions.module.scss';
 import {Button} from "../Common/components/button/Button";
 import {Title} from "../Common/components/title/Title";
+import Fade from 'react-reveal/Fade';
 
 function WorkingConditions() {
     return (
         <div className={s.workingBlock}>
-            <div className={`${sContainer.container} ${s.workingContainer}`}>
-                <Title title={'Сonsidering work options'}
-                       titleDiscription={'future description'}
-                />
-                <Button name={'Hire me'} />
+            <div className={s.container}>
+                <Fade left>
+                    <Title title={'Сonsidering work options'}
+                           titleDiscription={'future description'}
+                    />
+                    <Button name={'Hire me'} className={s.workingBtn}/>
+                </Fade>
             </div>
         </div>
     )
